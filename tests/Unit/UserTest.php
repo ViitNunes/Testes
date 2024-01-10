1df7c929938cf720dd96ab68c824c63308f25472
Ricardo <ricardoervilha456@gmail.com>
2024-01-10 11:33:13 -0300

    Testes C R U D

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Faker\Factory as Faker;
 use App\Models\User; // Certifique-se de importar o modelo User
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\AssertableJson;
 
 class UserTest extends \Tests\TestCase
 {
@@ -14,57 +16,70 @@ class UserTest extends \Tests\TestCase
     /**A basic unit test example.*/
     public function testCreation(): void{
     
-        $this->withoutMiddleware(); // Desabilita temporariamente os middlewares de autenticação // Desativa temporariamente os redirecionamentos
+        $this->withoutMiddleware(); //Desabilita os middlewares de autenticação
 
-        $faker = Faker::create();
+        $faker = Faker::create(); //Gera uma instância do Faker para criar dados falsos
 
+        //Array que guarda dados que serão usados para criar um usuário
         $payload = [
             'name' => $faker->firstName,
             'email' => $faker->email,
             'password' => $faker->password,
         ];
 
-        $response = $this->json('post', '/users', $payload);
-        $response->assertStatus(Response::HTTP_CREATED);
+        $response = $this->json('post', '/users', $payload); //Faz a requisição POST
+
+        $response->assertStatus(Response::HTTP_CREATED); //Compara se o status retornado é igual ao status de HTTP_CREATED(201)
+
+        $this->assertDatabaseHas('users', ['id' => $response->json()["user"]["id"]]); //Conferindo se o usuário realmente foi para a base de dados
     }
 
     public function testRead(): void
     {
         $this->withoutMiddleware();
-        $id = 1;
 
-        $response = $this->json('GET', "/users/{$id}");
-        $response->assertStatus(200);
-       // dd($response->json());
-       $expectedStructure = [
-        'user' => [
-            'id',
-            'name',
-            'email',
-            'password'
-           ]
-        ];
+        $id = User::all()->first()->id; //Pega o id do primeiro usuário do banco de dados
 
+        $response = $this->json('GET', "/users/{$id}"); //Faz a requisição
+
+        $response->assertStatus(200); //Compara se os status são ambos 200(Requisição bem sucedida)
+       
+        //Estrutura esperada do objeto json
+        $expectedStructure = [
+            'user' => [
+                'id',
+                'name',
+                'email',
+                'password'
+            ]
+            ];
+        
+        //Verifica se a estrutura está correta e é realmente a de um usuário
         $response->assertJsonStructure($expectedStructure);      
     }
 
     public function testUpdate(): void
     {
-        $user = User::factory(1)->create();
+        $user = User::factory()->create(); //Cria um usuário para teste
         
         $this->withoutMiddleware();
-        $id = 1;
+        
+        $id = $user->id;
 
         $faker = Faker::create();
 
+        //Dados de atualização
         $payload = [
             'name' => $faker->firstName,
             'email' => $faker->email,
             'password' => $faker->password,
         ];
 
-        $response = $this->json('put', "/users/{$id}", $payload);
-        $response->assertStatus(201);
+        $response = $this->json('put', "/users/{$id}", $payload); //Faz a requisição update
+        
+        $response->assertStatus(201); //Assegura o status 201
+        
+        //Garante que o objeto retornado tem essa estrutura
         $expectedStructure = [
         'user' => [
             'id',
@@ -73,32 +88,31 @@ class UserTest extends \Tests\TestCase
             'password'
            ]
         ];
-
+        
         $response->assertJsonStructure($expectedStructure); 
-        //$this->assertNotEquals($payload, json_decode($response->json), "o valor nao foi alterado");
-        // $response
-        // ->assertJson(fn (AssertableJson $json) =>
-        //     $json->where('id', $id)
-        //          ->where('name', $payload['name'])
-        //          ->where('email', fn (string $email) => str($email)->is($payload['email']))
-        //          ->where('password', $payload['password'])
-        // );
+        
+        //Verifica se os dados do payload batem com o que foi atualizado passando payload
+        $response
+        ->assertJson(fn (AssertableJson $json) =>
+            $json->where('user.id', $id)
+                 ->where('user.name', $payload["name"])
+                 ->where('user.email', $payload["email"])
+                //  ->where('user.password', bcrypt($payload["password"]))
+        );
     }
 
-    // public function testDelete(): void
-    // {
-    //     $user = User::factory()->create(); // Cria um usuário para ser deletado
-    //     $id = $user->id;
+    public function testDelete(): void
+    {
+        $this->withoutMiddleware();
 
-    //     $this->withoutMiddleware();
+        $user = User::factory()->create();
+        $id = $user->id;
 
-    //     $response = $this->json('delete', "/users/{$id}");
-    //     $response->assertStatus(Response::HTTP_NO_CONTENT);
+        $response = $this->json('delete', "/users/{$id}");  //Requisição de delete
 
-    // // Verifica se o usuário foi removido do banco de dados
-    //     $this->assertNull(User::find($id));
+        $response->assertStatus(204); //Status 204 (Tudo ok, sem content a retornar)
 
-    // }
-
+        $this->assertDatabaseMissing('users', ['id' => $id]); //Verifica se realmente não é possivel achar o id do usuário deletado
+    }
 
 }
